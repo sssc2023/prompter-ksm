83757b10ead8c4c8ea8d505c36f7687148776dfc
sssc2023 <143796470+sssc2023@users.noreply.github.com>
2023-09-01 20:26:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ if uploaded_file is not None:
             llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
             qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
             result = qa_chain({"query": question})
-            st.write(result["result"])v
+            st.write(result["result"])
