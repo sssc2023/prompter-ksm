b2552d3dbaa2f27426adf66f402aa5feb78aa33f
sssc2023 <143796470+sssc2023@users.noreply.github.com>
2023-09-02 00:39:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ if uploaded_file is not None:
     #Split
     text_splitter = RecursiveCharacterTextSplitter(
         # Set a really small chunk size, just to show.
-        chunk_size = 300,
-        chunk_overlap  = 20,
+        chunk_size = 800,
+        chunk_overlap  = 40,
         length_function = len,
         is_separator_regex = False,
     )
