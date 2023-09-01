8600668144698775d6778d0bae80d999c7b4053c
sssc2023 <143796470+sssc2023@users.noreply.github.com>
2023-09-01 20:44:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from st_click_detector import click_detector
 
 #제목
 st.title("ChatPDF")
 st.write("---")
 
+content = """<p><a href='#' id='Link 1'>First link</a></p>
+    <p><a href='#' id='Link 2'>Second link</a></p>
+    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
+    <a href='#' id='Image 2'><img width='20%' src='https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=200'></a>
+    """
+clicked = click_detector(content)
+
+st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
 #파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!",type=['pdf'])
 st.write("---")
